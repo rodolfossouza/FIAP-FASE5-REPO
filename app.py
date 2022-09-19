e4f606d377bb6b06b7d1a6f501a1a71d2420b8c9
rodolfossouza <rodolfo.ti@outlook.com>
2022-09-19 13:46:18 -0300

    corrigindo mensagem do app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO v02 - GRUPO 10"
+    return "Laboratório DevOps - FIAP 8ASO v02 - Grupo10"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
